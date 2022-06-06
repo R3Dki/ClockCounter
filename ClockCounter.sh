a2ba7df5038c035be22f8ff570092c94852158d4
R3Dki <106279482+R3Dki@users.noreply.github.com>
2022-06-06 15:14:59 +0200

    Add files via upload

--- ClockCounter.sh
+++ ClockCounter.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-MCAL=0
-REFVAL=0
-OUT1="Clock Frequency: "
-OUT2="GHz"
+#variables
+NOCOLOR=
 DELTA=0
 FREQUENCY=0
 #calibration
 OLDTIME=$(date +%s%N | cut -b1-13)
-while [[ $DELTA -le 1000 ]]; do
+while [[ $DELTA -lt 1000 ]]; do
   TIME=$(date +%s%N | cut -b1-13)
   DELTA=$(( $TIME-$OLDTIME ))
   FREQUENCY=$(( $FREQUENCY+1 ))
@@ -16,14 +14,27 @@ REFVAL=$((`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`/1000))
 MCAL=$(bc -l <<< "($REFVAL/$FREQUENCY)*1000000")
 #measurement
 while [[ TRUE ]]; do
-OLDTIME=$(date +%s%N | cut -b1-13)
-DELTA=0
-while [[ $DELTA -le 1000 ]]; do
-  TIME=$(date +%s%N | cut -b1-13)
-  DELTA=$(( $TIME-$OLDTIME ))
-  FREQUENCY=$(( FREQUENCY+1 ))
-done
-clear
-printf %s%.5f%s%s%s\\n "$OUT1" "$(bc -l <<< "$FREQUENCY*$MCAL" )e-9" "$OUT2"
-FREQUENCY=0
+  OLDTIME=$(date +%s%N | cut -b1-13)
+  DELTA=0
+  while [[ $DELTA -lt 1000 ]]; do
+    TIME=$(date +%s%N | cut -b1-13)
+    DELTA=$(( $TIME-$OLDTIME ))
+    FREQUENCY=$(( FREQUENCY+1 ))
+  done
+  REFVAL=$((`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`/1000))
+  clear
+  REFVAL=$(bc -l <<< "$REFVAL/$DELTA" )
+  FREQUENCY=$(bc -l <<< "($FREQUENCY*$MCAL)/1000000000" )
+  DELTA=$(bc -l <<< "($REFVAL-$FREQUENCY)*1000" )
+#colorcheck
+  if [[ $(echo "$DELTA > 100" | bc) -eq 1 ]]; then
+    COLOR=$'\e[0;31m'
+  elif [[ $(echo "$DELTA < -100" | bc) -eq 1 ]]; then
+    COLOR=$'\e[0;31m'
+  else
+    COLOR=$'\e[0;32m'
+  fi
+  #output
+  printf %s%s%.3f%s\\n%s%s%.3f%s\\n%s%.3f%s\\n "Clock Frequency: " $COLOR "$FREQUENCY" "GHz" $'\e[0m' "Reference Value: " "$REFVAL" "$GHz" "Difference: " "$DELTA" "MHz"
+  FREQUENCY=0
 done
