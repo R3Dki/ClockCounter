7c756728da32ef42a3015040afd20d90d00dedcd
R3Dki <106279482+R3Dki@users.noreply.github.com>
2023-03-06 21:59:17 +0100

    Create ClockCounter.ino

---
+++ ClockCounter.ino
@@ -0,0 +1,19 @@
+long sumCount = 0, prevTime = 0;
+int delta = 0;
+float frequency;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (delta < 1000) {
+    sumCount++;
+  }else{
+    frequency = sumCount*0.000053333;
+    Serial.println("ATMega Clock Frequency: " + (String)frequency + "MHz");
+    sumCount = 0;
+    prevTime = millis();
+  }
+  delta = millis()-prevTime;
+}
